5185139e92c55e8e8eba4698d26f240f34a687de
Rajesh Danala <rajesh.danala@gmail.com>
2015-09-25 16:42:49 -0400

    add ejson dependency

--- meteor-angular-todo.js
+++ meteor-angular-todo.js
@@ -4,7 +4,8 @@ if (Meteor.isClient) {
   // This code only runs on the client
   angular.module('simple-todos',['angular-meteor']);
  
-  angular.module('simple-todos').controller('TodosListCtrl', ['$scope','$meteor',
+  angular.module('simple-todos')
+        .controller('TodosListCtrl', ['$scope','$meteor',
     function ($scope,$meteor) {
  
       $scope.tasks = $meteor.collection( function() {
