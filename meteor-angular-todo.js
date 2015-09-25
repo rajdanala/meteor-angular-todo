87557160b39c962396485dde40bc077e8291850d
Rajesh Danala <rajesh.danala@gmail.com>
2015-09-25 16:05:19 -0400

    used latest meteor add  angular instead of meteor add  urigo:angular

--- meteor-angular-todo.js
+++ meteor-angular-todo.js
@@ -1,23 +1,20 @@
+Tasks = new Mongo.Collection('tasks');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  // This code only runs on the client
+  angular.module('simple-todos',['angular-meteor']);
+ 
+  angular.module('simple-todos').controller('TodosListCtrl', ['$scope','$meteor',
+    function ($scope,$meteor) {
+ 
+      $scope.tasks = $meteor.collection( function() {
+        return Tasks.find({}, { sort: { createdAt: -1 } })
+      });
+      $scope.addTask = function (newTask) {
+        $scope.tasks.push( {
+          text: newTask,
+          createdAt: new Date() }
+        );
+      };
+  }]);
 }
